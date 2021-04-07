8eeddf34966f180b135bb554e95044f40f61e6e4
Evgeny Kulikov <evgeny.kulikov@outlook.com>
2021-04-07 16:33:57 +0300

    remove options for babel-loader (allow babel to read config from `babel.config.json` file)

--- extra-lessons/babel-webpack/webpack.config.js
+++ extra-lessons/babel-webpack/webpack.config.js
@@ -11,12 +11,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: 'babel-loader',
       },
     ],
   },
