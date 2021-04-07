01929e006ab2bf1a8c88fd6b63768b53d8de4f9d
Evgeny Kulikov <evgeny.kulikov@outlook.com>
2021-04-07 09:47:16 +0300

    webpack

---
+++ lessons/babel-webpack/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
