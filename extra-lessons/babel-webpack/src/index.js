9f73fdc6be7968c0dafc3299b5620a5524c5d7a5
Evgeny Kulikov <evgeny.kulikov@outlook.com>
2021-04-07 17:47:30 +0300

    - setup experimental pipeline operator support

--- extra-lessons/babel-webpack/src/index.js
+++ extra-lessons/babel-webpack/src/index.js
@@ -3,4 +3,7 @@ import { add1 } from './modules/add1';
 import { pow4 } from './modules/pow4';
 import { multiply3 } from './modules/multiply3';
 
-console.log(compose(multiply3, pow4, add1)(1));
+console.log('result using compose function', compose(multiply3, pow4, add1)(1));
+
+// То же самое, используя pipeline operator
+console.log('result using pipeline operator', 1 |> add1 |> pow4 |> multiply3);
