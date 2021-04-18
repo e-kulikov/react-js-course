7c0e739f2b242cbec5a79ad87b3461da1a6b82a2
Evgeny Kulikov <evgeny.kulikov@outlook.com>
2021-04-18 17:49:34 +0300

    lesson summary

---
+++ extra-lessons/js/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[fullhash:6].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HTMLWebpackPlugin()],
+};
